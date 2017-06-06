aa263c30149b11ace9e0e2f696e0309f789d466f
Tommaso Poletti <polettitommaso@gmail.com>
2017-06-06 12:17:03 +0200

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,77 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the necessary extensibility types to use in your code below
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+
+// This method is called when your extension is activated. Activation is
+// controlled by the activation events defined in package.json.
+export function activate(context: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error).
+    // This line of code will only be executed once when your extension is activated.
+    console.log('Congratulations, your extension "SassMap" is now active!');
+
+    // create a new word counter
+    let wordCounter = new WordCounter();
+
+    let disposable = commands.registerCommand('extension.sayHello', () => {
+        wordCounter.updateWordCount();
+    });
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(disposable);
+}
+
+class WordCounter {
+
+    private _statusBarItem: StatusBarItem;
+
+    public updateWordCount() {
+
+        // Create as needed
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        // Get the current text editor
+        let editor = window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+
+        // Only update status if an Markdown file
+        if (doc.languageId === "markdown") {
+            let wordCount = this._getWordCount(doc);
+
+            // Update the status bar
+            this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+            this._statusBarItem.color = "#fff";
+            this._statusBarItem.command = "";
+            this._statusBarItem.show();
+        } else { 
+            this._statusBarItem.hide();
+        }
+    }
+
+    public _getWordCount(doc: TextDocument): number {
+
+        let docContent = doc.getText();
+
+        // Parse out unwanted whitespace so the split is accurate
+        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        let wordCount = 0;
+        if (docContent != "") {
+            wordCount = docContent.split(" ").length;
+        }
+
+        return wordCount;
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
+}
